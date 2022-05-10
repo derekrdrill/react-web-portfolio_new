3f19bbfb8ab60b8654b5bb90399f0782398acf76
Derek Drill <derekrdrill@gmail.com>
2022-05-10 10:24:16 -0400

    incorporating lead input context into the LeadInputPage component

--- src/components/LeadInputForm/context/LeadInputReducer.js
+++ src/components/LeadInputForm/context/LeadInputReducer.js
@@ -1,5 +1,16 @@
 export const leadInputReducer = (state, action) => {
   switch (action.type) {
+    case 'SWITCH_PAGES':
+      return {
+        ...state,
+        page: action.payload.page,
+        tooltipOpen: action.payload.tooltipOpen,
+      };
+    case 'TOGGLE_TOOLTIP':
+      return {
+        ...state,
+        tooltipOpen: action.payload,
+      };
     default:
       return state;
   }
