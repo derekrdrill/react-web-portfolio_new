f6f39faba2c6e9463584f83eb63c492c4380a825
Derek Drill <derekrdrill@gmail.com>
2022-05-10 12:27:43 -0400

    creating sandbox component

---
+++ src/components/Sandbox/Sandbox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UseRefExample1 } from './components/UseRefExample1';
+
+export const Sandbox = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <UseRefExample1 />
+    </div>
+  );
+};
