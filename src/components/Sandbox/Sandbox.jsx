b5bfe78474010728c920c49f11232656cdd1550a
Derek Drill <derekrdrill@gmail.com>
2022-05-10 16:43:59 -0400

    UseMemoExample

--- src/components/Sandbox/Sandbox.jsx
+++ src/components/Sandbox/Sandbox.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { UseRefExample1 } from './components/UseRefExample1';
 import { UseRefExample2 } from './components/UseRefExample2';
+import { UseRefExample3 } from './components/UseRefExample3';
+import { UseMemoExample } from './components/UseMemoExample';
 
 export const Sandbox = () => {
   return (
     <div style={{ padding: 20 }}>
-      <UseRefExample2 />
+      <UseMemoExample />
     </div>
   );
 };
