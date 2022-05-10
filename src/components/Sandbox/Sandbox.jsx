58a898d99fbdc2db83798c2895197839810f2b04
Derek Drill <derekrdrill@gmail.com>
2022-05-10 13:24:49 -0400

    UseRef Example 2

--- src/components/Sandbox/Sandbox.jsx
+++ src/components/Sandbox/Sandbox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { UseRefExample1 } from './components/UseRefExample1';
+import { UseRefExample2 } from './components/UseRefExample2';
 
 export const Sandbox = () => {
   return (
     <div style={{ padding: 20 }}>
-      <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 };
