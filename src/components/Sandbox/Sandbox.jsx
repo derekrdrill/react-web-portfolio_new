8b898beea9419237745710496ba7cecc41235851
Derek Drill <derekrdrill@gmail.com>
2022-05-12 10:22:06 -0400

    Custom hook example 2: useLocalStorge

--- src/components/Sandbox/Sandbox.jsx
+++ src/components/Sandbox/Sandbox.jsx
@@ -5,11 +5,12 @@ import { UseRefExample3 } from './components/UseRefExample3';
 import { UseMemoExample } from './components/UseMemoExample';
 import { UseCallbackExample } from './components/UseCallbackExample';
 import { CustomHookExample1 } from './components/CustomHookExample1';
+import { CustomHookExample2 } from './components/CustomHookExample2';
 
 export const Sandbox = () => {
   return (
     <div style={{ padding: 20 }}>
-      <CustomHookExample1 />
+      <CustomHookExample2 />
     </div>
   );
 };
