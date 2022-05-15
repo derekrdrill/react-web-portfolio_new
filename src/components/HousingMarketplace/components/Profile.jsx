0343b601f74d77ab64332f85d339eef5c72d69d8
Derek Drill <derekrdrill@gmail.com>
2022-05-15 19:10:49 -0400

    building user profile component

--- src/components/HousingMarketplace/components/Profile.jsx
+++ src/components/HousingMarketplace/components/Profile.jsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import { history } from '../../../index';
+import styled from 'styled-components';
+import { Grid, Typography } from '@mui/material';
+import { Card } from '../../Card/Card';
 
 export const Profile = () => {
-  return <div>Profile</div>;
+  const token = sessionStorage.getItem('token');
+  !token && history.push('/housing-marketplace/auth');
+
+  const email = sessionStorage.getItem('email');
+  const firstName = sessionStorage.getItem('firstName');
+  const lastName = sessionStorage.getItem('lastName');
+
+  return (
+    <MainContainer>
+      <TitleContainer>
+        <Typography component='h6' variant='h4'>
+          My Profile
+        </Typography>
+      </TitleContainer>
+      <Typography component='h6' variant='h6'>
+        Personal Details
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Card backgroundColor='gainsboro' spacing={false}>
+            <Typography component='h6' variant='subtitle1'>
+              {`${firstName} ${lastName}`}
+            </Typography>
+            <Typography component='h6' variant='subtitle2'>
+              {email}
+            </Typography>
+          </Card>
+        </Grid>
+      </Grid>
+    </MainContainer>
+  );
 };
+
+const MainContainer = styled.div({
+  padding: 20,
+});
+
+const TitleContainer = styled.div({
+  padding: '10px 0',
+});
+
