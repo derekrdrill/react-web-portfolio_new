be680077c6770dd4de2a20deac7affc6a4364856
Derek Drill <derekrdrill@gmail.com>
2022-05-15 19:08:29 -0400

    updates to card to be used in the user profile

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,26 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export const Card = ({ children, darkMode }) => <StyledDiv darkMode={darkMode}>{children}</StyledDiv>;
+export const Card = ({ backgroundColor, children, darkMode, spacing, textColor }) => (
+  <StyledDiv backgroundColor={backgroundColor} darkMode={darkMode} spacing={spacing} textColor={textColor}>
+    {children}
+  </StyledDiv>
+);
 
 Card.propTypes = {
+  backgroundColor: PropTypes.string,
   children: PropTypes.node,
   darkMode: PropTypes.bool,
+  padding: PropTypes.bool,
+  spacing: PropTypes.bool,
+  textColor: PropTypes.string,
 };
 
 Card.defaultProps = {
   darkMode: false,
+  padding: true,
 };
 
-const StyledDiv = styled.div(({ darkMode }) => ({
+const StyledDiv = styled.div(({ backgroundColor, darkMode, spacing, textColor }) => ({
   '.MuiInputBase-root': {
-    backgroundColor: darkMode ? '#404040' : '#FFF',
-    color: darkMode ? '#c7c7c7' : '#333',
+    backgroundColor: darkMode ? '#404040' : backgroundColor ? backgroundColor : '#FFF',
+    color: darkMode ? '#fff' : textColor ? textColor : '#333',
   },
-  backgroundColor: darkMode ? '#333' : '#fff',
-  color: darkMode ? '#fff' : '#333',
+  backgroundColor: darkMode ? '#404040' : backgroundColor ? backgroundColor : '#FFF',
+  color: darkMode ? '#fff' : textColor ? textColor : '#333',
   borderRadius: 15,
-  padding: '40px 50px',
-  margin: '20px 25px',
+  padding: spacing ? '40px 50px' : 10,
+  margin: spacing ? '20px 25px' : 2,
   position: 'relative',
 }));
