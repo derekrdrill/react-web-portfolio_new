6ad371df7c74674327a34cae1ffa20b7748fdd56
Derek Drill <derekrdrill@gmail.com>
2022-07-12 21:20:34 -0400

    changes to the resume options modal

--- src/components/Header/components/MoreOptionsModal.jsx
+++ src/components/Header/components/MoreOptionsModal.jsx
@@ -1,52 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Box, Button, Modal, Typography } from '@mui/material/';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faFilePdf } from '@fortawesome/fontawesome-free-solid';
 import { faGoogleDrive } from '@fortawesome/free-brands-svg-icons';
+
+import { DarkLightModeContext } from '../../DarkLightMode/context/DarkLightModeContext';
+
 import resume from '../../../assets/Resume_DRD.pdf';
 
-export const MoreOptionsModal = ({ open, handleModalClose, quickViewOpen }) => (
-  <Modal open={open} onClose={handleModalClose}>
-    <StyledBox>
-      <Typography variant='h6' component='h2'>
-        Resume options
-      </Typography>
-      <Typography sx={{ mt: 2 }}>Select one of the following:</Typography>
-      <StyledLink href={resume} download>
-        <ResumeOptionButtons>
-          Download a Copy
-          <ResumeOptionsButtonIcon icon={faFilePdf} />
-        </ResumeOptionButtons>
-      </StyledLink>
-      <StyledLink
-        href='https://docs.google.com/document/d/1qJIlaZflsm-oBC34z5-z2aLWVnZJJZkz/edit?usp=sharing&ouid=111233393235096509511&rtpof=true&sd=true'
-        target='_blank'
-      >
-        <ResumeOptionButtons>
-          Go to Google Doc
-          <ResumeOptionsButtonIcon icon={faGoogleDrive} />
-        </ResumeOptionButtons>
-      </StyledLink>
-      <ResumeOptionButtons onClick={quickViewOpen}>
-        Quick View
-        <ResumeOptionsButtonIcon icon={faEye} />
-      </ResumeOptionButtons>
-      <ResumeOptionButtons cancelbutton='true' onClick={handleModalClose}>
-        Cancel
-      </ResumeOptionButtons>
-    </StyledBox>
-  </Modal>
-);
+export const MoreOptionsModal = ({ open, handleModalClose, quickViewOpen }) => {
+  const { darkMode } = useContext(DarkLightModeContext);
 
-const StyledBox = styled(Box)({
-  backgroundColor: 'white',
+  return (
+    <Modal open={open} onClose={handleModalClose}>
+      <StyledBox darkMode={darkMode}>
+        <Typography variant='h6' component='h2'>
+          Resume options
+        </Typography>
+        <Typography sx={{ mt: 2 }}>Select one of the following:</Typography>
+        <StyledLink href={resume} download>
+          <ResumeOptionButtons>
+            Download a Copy
+            <ResumeOptionsButtonIcon icon={faFilePdf} />
+          </ResumeOptionButtons>
+        </StyledLink>
+        <StyledLink
+          href='https://docs.google.com/document/d/1qJIlaZflsm-oBC34z5-z2aLWVnZJJZkz/edit?usp=sharing&ouid=111233393235096509511&rtpof=true&sd=true'
+          target='_blank'
+        >
+          <ResumeOptionButtons>
+            Go to Google Doc
+            <ResumeOptionsButtonIcon icon={faGoogleDrive} />
+          </ResumeOptionButtons>
+        </StyledLink>
+        <ResumeOptionButtons onClick={quickViewOpen}>
+          Quick View
+          <ResumeOptionsButtonIcon icon={faEye} />
+        </ResumeOptionButtons>
+        <ResumeOptionButtons cancelbutton='true' onClick={handleModalClose}>
+          Cancel
+        </ResumeOptionButtons>
+      </StyledBox>
+    </Modal>
+  );
+};
+
+const StyledBox = styled(Box)(({ darkMode }) => ({
+  'h1, h2, h3, h4, h5, h6, p': { color: darkMode && '#c4c4c4' },
+  backgroundColor: darkMode ? '#545454' : 'white',
   marginLeft: '25%',
   marginTop: '18%',
   maxWidth: '50%',
   padding: 20,
   borderRadius: 5,
-});
+}));
 
 export const ResumeOptionButtons = styled(Button)(({ cancelbutton }) => ({
   backgroundColor: cancelbutton ? 'gainsboro' : '#311b92',
