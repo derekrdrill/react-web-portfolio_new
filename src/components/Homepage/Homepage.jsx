a4cb89a088334881d9dfc8f0f9c0e030c64876a6
Derek Drill <derekrdrill@gmail.com>
2022-10-04 15:56:48 -0400

    Homepage unit testing changes

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,40 +1,49 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Grid, Typography } from '@mui/material';
 
 import { DarkLightModeContext } from '../DarkLightMode/context/DarkLightModeContext';
 
 import homepagePhoto from '../../assets/homepagePhoto.jpeg';
-import qcLogoWhite from '../../assets/crown-white.png';
 import qcLogoBlue from '../../assets/crown-blue.png';
+import qcLogoWhite from '../../assets/crown-white.png';
+
+export const getQCLogo = (darkMode, qcLogoWhite, qcLogoBlue) =>
+  darkMode ? qcLogoWhite : qcLogoBlue;
 
 export const Homepage = ({ id }) => {
-  const { darkMode } = useContext(DarkLightModeContext);
+  const { darkMode } = React.useContext(DarkLightModeContext);
 
   return (
     <>
       <PageBodyStyle darkMode={darkMode} />
-      <HomepageContainer id={id} container darkMode={darkMode} justifyContent={{ xs: 'center', md: 'flex-start' }}>
+      <HomepageContainer
+        id={id}
+        container
+        darkMode={darkMode}
+        justifyContent={{ xs: 'center', md: 'flex-start' }}
+      >
         <Grid item order={{ xs: 2, md: 1 }} xs={12} md={9}>
           <Grid container justifyContent={{ xs: 'center', sm: 'flex-start' }}>
             <Grid item xs={12} sm={9} md={7} lg={6} xl={5}>
               <Grid container display={{ xs: 'block', md: 'none' }}>
-                <QCLogo alt='qc-logo' src={darkMode ? qcLogoWhite : qcLogoBlue} />
+                <QCLogo alt='qc-logo' src={getQCLogo(darkMode, qcLogoWhite, qcLogoBlue)} />
               </Grid>
               <HomePageText darkMode={darkMode} variant='h2' component='h1'>
                 Welcome from the Queen City
               </HomePageText>
             </Grid>
             <Grid item sm={9} md={5} lg={6} xl={7} display={{ xs: 'none', md: 'block' }}>
-              <QCLogo src={darkMode ? qcLogoWhite : qcLogoBlue} alt='qc-logo' />
+              <QCLogo src={getQCLogo(darkMode, qcLogoWhite, qcLogoBlue)} alt='qc-logo' />
             </Grid>
             <Grid item xs={12} justifyContent={{ xs: 'center', sm: 'flex-start' }}>
               <HomePageText darkMode={darkMode} variant='h4' component='h1'>
-                I'm Derek! A passionate web developer and the G.O.A.T. dog-dad of the Greater Charlotte Metro area
+                I'm Derek! A passionate web developer and the G.O.A.T. dog-dad of the Greater
+                Charlotte Metro area
               </HomePageText>
               <HomePageText darkMode={darkMode} variant='h6' component='h1'>
-                I strive to elegantly code user-friendly web apps that utilize React, Material UI and Styled-Components
-                in the front-end, and MongoDB and ExpressJS in the back-end
+                I strive to elegantly code user-friendly web apps that utilize React, Material UI
+                and Styled-Components in the front-end, and MongoDB and ExpressJS in the back-end
               </HomePageText>
             </Grid>
           </Grid>
@@ -47,31 +56,30 @@ export const Homepage = ({ id }) => {
   );
 };
 
-const PageBodyStyle = createGlobalStyle(({ darkMode }) => ({
+export const PageBodyStyle = createGlobalStyle(({ darkMode }) => ({
   body: {
     backgroundColor: darkMode ? '#030200' : 'whitesmoke',
   },
 }));
 
-const HomepageContainer = styled(Grid)({
+export const HomepageContainer = styled(Grid)({
   padding: '200px 50px 500px 50px',
 });
 
-
-const HomePageText = styled(Typography)(({ darkMode }) => ({
+export const HomePageText = styled(Typography)(({ darkMode }) => ({
   marginBottom: 15,
   fontFamily: 'Roboto Slab',
   color: darkMode ? 'white' : '#759CC9',
 }));
 
-const HomePageImage = styled.img({
+export const HomePageImage = styled.img({
   width: 325,
   height: 350,
   borderRadius: 250,
   border: '2px royalblue dotted',
 });
 
-const QCLogo = styled.img({
+export const QCLogo = styled.img({
   marginBottom: 10,
   width: 150,
   height: 100,
--- src/components/Homepage/Homepage.test.js
+++ src/components/Homepage/Homepage.test.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 
-import { Homepage } from './Homepage';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import { Homepage, HomePageText } from './Homepage';
+import { getQCLogo } from './Homepage';
+
+import qcLogoBlue from '../../assets/crown-blue.png';
+import qcLogoWhite from '../../assets/crown-white.png';
+
+configure({ adapter: new Adapter() });
 
 const renderer = new ShallowRenderer();
 
 describe('Homepage tests', () => {
+  let realUseContext;
+  let useContextMock;
+
+  beforeEach(() => {
+    realUseContext = React.useContext;
+    useContextMock = React.useContext = jest.fn();
+  });
+
+  afterEach(() => {
+    React.useContext = realUseContext;
+  });
+
   it('renders correctly', () => {
+    useContextMock.mockReturnValue({ darkMode: true });
+
     renderer.render(<Homepage />);
     const result = renderer.getRenderOutput();
     expect(result).toMatchSnapshot();
+  });
+
+  it('renders HomePageText styled component correctly', () => {
+    const homePageText = shallow(<HomePageText />);
+    const homePageTextDark = shallow(<HomePageText darkMode />);
+
+    expect(homePageText.props().className).toEqual('sc-gsnTZi jJaOCK');
+    expect(homePageTextDark.props().className).toEqual('sc-gsnTZi IVrlM');
+  });
+
+  it('runs getQCLogo', () => {
+    expect(getQCLogo(true, qcLogoWhite, qcLogoBlue)).toEqual(qcLogoWhite);
+    expect(getQCLogo(false, qcLogoWhite, qcLogoBlue)).toEqual(qcLogoBlue);
   });
 });
