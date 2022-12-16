3bbc9093f039d8c384e107a7c5306aa7fd174276
Derek Drill <derekrdrill@gmail.com>
2022-12-15 21:17:43 -0500

    applying darkMode to GithubUserItem component

--- src/components/GithubFinder/components/GithubUserItem.jsx
+++ src/components/GithubFinder/components/GithubUserItem.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Typography } from '@mui/material';
 
+import { DarkLightModeContext } from '../../DarkLightMode/context/DarkLightModeContext';
 import { GithubContext } from '../context/GithubContext';
 
 import { getUserAndRepos } from '../context/GithubActions';
 
 export const GithubUserItem = ({ user: { login, avatar_url } }) => {
+  const { darkMode } = React.useContext(DarkLightModeContext);
   const { githubDispatch } = React.useContext(GithubContext);
 
   const handleGetUser = async () => {
@@ -18,7 +20,7 @@ export const GithubUserItem = ({ user: { login, avatar_url } }) => {
   };
 
   return (
-    <UserItemContainer>
+    <UserItemContainer darkMode={darkMode}>
       <UserImage src={avatar_url} />
       <div>
         <Typography variant='subtitle1' component='h6'>
@@ -38,14 +40,14 @@ GithubUserItem.propTypes = {
   user: PropTypes.object,
 };
 
-const UserItemContainer = styled.div({
+const UserItemContainer = styled.div(({ darkMode }) => ({
   borderRadius: 10,
-  boxShadow: '2px 1px 10px #121326',
+  boxShadow: darkMode ? '2px 1px 10px #121326' : '1px 1px 5px #121326',
   display: 'flex',
   justifyContent: 'space-around',
   margin: 10,
   padding: 5,
-});
+}));
 
 const UserImage = styled.img({
   borderRadius: 50,
