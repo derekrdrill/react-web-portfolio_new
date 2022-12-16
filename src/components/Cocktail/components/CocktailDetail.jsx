799149da43b756eddf023cff81eb4c31efca0f55
Derek Drill <derekrdrill@gmail.com>
2022-12-16 12:53:30 -0500

    Building Cocktail app components

---
+++ src/components/Cocktail/components/CocktailDetail.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Grid, Typography } from '@mui/material';
+
+import { DarkLightModeContext } from '../../DarkLightMode/context/DarkLightModeContext';
+import { CocktailContext } from '../../Cocktail/context/CocktailContext';
+
+const CocktailDetail = () => {
+  const { cocktailDispatch, searchResults, searchType } = React.useContext(CocktailContext);
+  const { darkMode } = React.useContext(DarkLightModeContext);
+
+  console.log(searchResults);
+
+  return (
+    searchResults &&
+    searchResults.map((searchResult, searchResultKey) => (
+      <CocktailDetailContainer key={searchResultKey} container>
+        <Grid item xs={2} lg={2} />
+        <Grid item xs={10} lg={3}>
+          <CocktailImage src={searchResult.strDrinkThumb} />
+        </Grid>
+        <Grid item xs={2} lg={1} />
+        <Grid item xs={8} lg={4}>
+          <Typography variant='h3'>{searchResult.strDrink}</Typography>
+          <br />
+          <Typography variant='h5'>Ingredients</Typography>
+          <ul>
+            {Object.keys(searchResult)
+              .filter(result => result.includes('strIngredient'))
+              .map(
+                (ingredient, ingredientKey) =>
+                  searchResult[ingredient] && (
+                    <li key={`${searchResult.idDrink}${ingredientKey}`}>
+                      <Typography variant='body1'>
+                        {`${searchResult[`strMeasure${ingredientKey + 1}`] ?? ''} ${
+                          searchResult[ingredient]
+                        }`}
+                      </Typography>
+                    </li>
+                  ),
+              )}
+          </ul>
+          <Typography variant='h5'>Instructions</Typography>
+          <Typography variant='body2'>{searchResult.strInstructions}</Typography>
+        </Grid>
+      </CocktailDetailContainer>
+    ))
+  );
+};
+
+CocktailDetail.propTypes = {};
+
+export default CocktailDetail;
+
+export const CocktailDetailContainer = styled(Grid)({
+  marginBottom: 150,
+  position: 'relative',
+  top: 125,
+});
+
+export const CocktailImage = styled.img({
+  borderRadius: 30,
+  height: 300,
+  width: 350,
+});
