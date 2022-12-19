b608b6aad04debd79e1913d783e4e18596b00d13
Derek Drill <derekrdrill@gmail.com>
2022-12-18 22:56:41 -0500

    test commit

--- src/components/Cocktail/context/CocktailActions.js
+++ src/components/Cocktail/context/CocktailActions.js
@@ -117,6 +117,7 @@ export const getCocktailsWithYoutubeData = async (cocktail, cocktailDispatch) =>
     },
   };
 
+  // result
   const result = await axios(options).catch(e => console.log(e));
 
   cocktail.youtubeData = result.data.items[0];
